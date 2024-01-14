9eff73f10b6cf7f85b20bfbd2d93b6a35b72881d
YuliaKravchukS <ljuljek554@gmail.com>
2024-01-14 15:26:24 +0200

    update ht 9/2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,16 +1,31 @@
 const form = document.querySelector(".feedback-form");
 const textarea = form.elements.message;
+const input = form.elements.email;
 const localStorageKey = "feedback-form-state";
 
-textarea.value = localStorage.getItem(localStorageKey) ?? "";
+
+const savedData = JSON.parse(localStorage.getItem(localStorageKey)) || {};
+textarea.value = savedData.textarea || "";
+input.value = savedData.input || "";
 
 form.addEventListener("input", (evt) => {
-  localStorage.setItem(localStorageKey, evt.target.value);
+  const value = {
+    input: input.value,
+    textarea: textarea.value,
+  };
+
+localStorage.setItem(localStorageKey, JSON.stringify(value));
+
+const savedSettings = localStorage.getItem(localStorageKey);
+
+const parsedSettings = JSON.parse(savedSettings);
 });
 
 form.addEventListener("submit", (evt) => {
   evt.preventDefault();
-	console.log(evt.target.elements.message.value);
+  console.log("Email:", evt.target.elements.email.value);
+  console.log("Message:", evt.target.elements.message.value);
+
   localStorage.removeItem(localStorageKey);
   form.reset();
 });
